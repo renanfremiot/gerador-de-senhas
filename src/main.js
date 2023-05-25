6f0a18be87d54344e75c0dd29a2c823c3930046d
Renan Fremiot <renanfremiot@gmail.com>
2023-05-25 15:06:00 -0400

    Gerador de senhas Ok

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
-console.log('Hello World!')
+import { nanoid } from 'nanoid';
+import './style.css';
+import copy from 'clipboard-copy';
+
+const passwordButton = document.querySelector('button');
+const displayPassword = document.querySelector('h2');
+
+passwordButton.addEventListener('click', () => {
+  const randomPassword = nanoid();
+  displayPassword.innerHTML = randomPassword;
+});
+
+displayPassword.addEventListener('click', () => {
+  copy(displayPassword.innerHTML);
+  alert('Copiou a senha!');
+});
